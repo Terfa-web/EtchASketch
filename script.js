63ce93fe1d8fdb27b3323608bc9e80178e9e1768
NOMHWANGE <afathelion@gmail.com>
2023-07-05 12:54:17 +0300

    Modifications
    Removed the back ground colors of grid divs
    
    created transferproperty function and added event listers
    on buttons with class names  of pinkcolor and multicolor

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
 
 
-//create the div container
+//create child div container and assign css prop
 const container = document.createElement('div')
+      container.classList.add('container')
+      container.style.width = '100%';
+      container.style.display = 'flex';
+      container.style.flexWrap = 'wrap';
+ 
+//select the parent div pad from the document 
 const pad = document.querySelector('.pad');
 
 //divide the container into default grid size
-container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-container.style.gridTemplateRows = 'repeat(16,1fr)';
-
-//append the container to parent div 
+ let cell;
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+     cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.width = 'calc(100%/16)';
+    cell.style.height = 'calc(100%/16)';
+    container.appendChild(cell);
+  }
+  
+}
 pad.appendChild(container);
 
-//assign listener to each node of the container divs
-//const tranferProperty = (e) => {
 
-//Array.from(padDivs).forEach(div => {
-  //div.addEventListener('click', {
-  //take the this.color prop and assing it to the div assignProp);
-// // this.classList.add(e.button.multicolor)
-//});
-//})
-//}
 
 //select the buttons from html document
 const gridsize = document.querySelector('.gridsize');
 const multi = document.querySelector('.multicolor')
+const pink = document.querySelector('.pinkcolor')
 const clear = document.querySelector('.clear');
 
-
+ 
 //create the function to change grid size
+
 const changeGrid = () => {
   const userInput = prompt('Enter the value for grid size:');
-  container.style.gridTemplateColumns= `repeat(${userInput}, 1fr)`;
-  container.style.gridTemplateRows= `repeat(${userInput}, 1fr)`
+  const num = parseInt(userInput);
+  container.innerHTML = '';
+ 
+  let newCell;
+for (let i = 0; i < num; i++) {
+  for (let j = 0; j < num; j++) {
+     newCell = document.createElement("div");
+    newCell.classList.add("newcell");
+    newCell.style.width = `calc(100%/${num})`;
+    newCell.style.height = `calc(100%/${num})`;
+  
+    container.appendChild(newCell);
+  }  
 }
 
-//create functon to clear the grid
+
+ pad.appendChild(container); 
+}
+
+//create function to clear the grid
 const clearGrid = () => {
-  container.innerHTML = "";
+ container.innerHTML = '';
 }
 
 //add an event listeners to buttons
@@ -45,19 +66,26 @@ gridsize.addEventListener('click', changeGrid)
 
 clear.addEventListener('click', clearGrid)
 
-const tranferProperty = (e) => {
-  const computedStyle = window.getComputedStyle(e.target);
-  
-  
-  const backgroundColor = computedStyle.getPropertyValue('background');
 
-  //get the grid divs from the sketch pad
-  const padDivs = document.querySelectorAll('.pad');
+const tranferProperty = (event) => {
+  const buttonClassName = event.target.className;
 
-  // convert the grid nodes to array and loop through each
+  const container = document.querySelectorAll('.container > div');
+  //loop through each node in the container 
+  //first convert the nodes to an array lis
+  Array.from(container).forEach(div => {
+    div.addEventListener('mouseover', (e) => {
+      e.target.classList.add(buttonClassName);
+    }, {
+      
+    })
+  })
   
-  console.log(backgroundColor);
-}
+ 
+ 
+};
+
 
 multi.addEventListener('click', tranferProperty);
+pink.addEventListener('click', tranferProperty);
 
